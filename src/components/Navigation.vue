f5a998ef2484b0080ad1296e6ae561513825277e
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2018-12-11 21:10:34 +0100

    Update Navigation Layout 🧭

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,13 +1,50 @@
 <template>
-  <v-toolbar flat app>
-    <v-toolbar-side-icon class="grey--text"/>
-    <v-toolbar-title class="text--uppercase grey--text">
-      <span>Vue</span>
-      <span class="font-weight-light">llet</span>
-    </v-toolbar-title>
-  </v-toolbar>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-title class="text--uppercase grey--text">
+        <span>Vue</span>
+        <span class="font-weight-light">llet</span>
+      </v-toolbar-title>
+      <v-spacer/>
+      <v-toolbar-items>
+        <ToolbarItem v-for="link in links" :key="link.title" :title="link.title" :icon="link.icon"/>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-navigation-drawer permanent app mini-variant>
+      <v-toolbar flat/>
+      <v-list dir class="mt-2 pt-0">
+        <v-list-tile v-for="link in links" :key="link.title" @click>
+          <v-list-tile-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
-export default {};
+import ToolbarItem from "./ToolbarItem";
+export default {
+  components: { ToolbarItem },
+  data() {
+    return {
+      // TODO: Add actual links
+      links: [
+        {
+          title: "Create Wallet",
+          icon: "mdi-pencil"
+        },
+        {
+          title: "Add Goal",
+          icon: "mdi-plus-circle"
+        },
+        {
+          title: "Settings",
+          icon: "mdi-settings"
+        }
+      ]
+    };
+  }
+};
 </script>
---
+++ src/components/ToolbarItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn flat color="grey mr-2">
+    <v-icon left>{{ icon ? icon : null }}</v-icon>
+    <span>{{ title }}</span>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String
+    }
+  }
+};
+</script>
